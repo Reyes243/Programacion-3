319ac7c698f980abaef3e88d041b6b19af865537
Reyes243 <josereyesh_23@alu.uabcs.mx>
2025-04-30 08:32:23 -0700

    Ejercicio 5 (Corregido)

--- mvc/views/AuthView.java
+++ mvc/views/AuthView.java
@@ -188,57 +188,57 @@ public void login() {
 	
 	JLabel etiqueta1 = new JLabel("Registro de usuario");
 	etiqueta1.setSize(200,60);
-	etiqueta1.setLocation(250, 30);
+	etiqueta1.setLocation(350, 0);
 	etiqueta1.setHorizontalAlignment(JLabel.CENTER);
 	etiqueta1.setFont(new Font("Corbel", Font.BOLD, 24));	
 	ventanaPanel.add(etiqueta1);
 	
 	JLabel etiqueta2 = new JLabel("Nombre de usuario:");
-	etiqueta2.setSize(200, 30);
-	etiqueta2.setLocation(240, 100);
-	etiqueta2.setFont(new Font("Corbel", Font.BOLD, 18));	
+	etiqueta2.setSize(200, 43);
+	etiqueta2.setLocation(500, 53);
+	etiqueta2.setFont(new Font("Tahoma", Font.PLAIN, 14));	
 	ventanaPanel.add(etiqueta2);
 	
 	JTextField usuario = new JTextField();
-	usuario.setSize(290, 30);
-	usuario.setLocation(240, 130);
-	usuario.setFont(new Font("Corbel", Font.BOLD, 18));
+	usuario.setSize(200, 19);
+	usuario.setLocation(500, 106);
+	usuario.setFont(new Font("Tahoma", Font.PLAIN, 14));
 	ventanaPanel.add(usuario);
 	
 	JLabel etiqueta3 = new JLabel("Correo electronico:");
-	etiqueta3.setSize(200, 30);
-	etiqueta3.setLocation(240, 170);
-	etiqueta3.setFont(new Font("Corbel", Font.BOLD, 18));	
+	etiqueta3.setSize(200, 43);
+	etiqueta3.setLocation(500, 140);
+	etiqueta3.setFont(new Font("Tahoma", Font.PLAIN, 14));	
 	ventanaPanel.add(etiqueta3);
 	
 	JTextField email = new JTextField();
-	email.setSize(290, 30);
-	email.setLocation(240, 200);
-	email.setFont(new Font("Corbel", Font.BOLD, 18));	
+	email.setSize(200, 19);
+	email.setLocation(500, 185);
+	email.setFont(new Font("Tahoma", Font.PLAIN, 14));
 	ventanaPanel.add(email);
 	
 	JLabel etiqueta4 = new JLabel("Contraseña:");
-	etiqueta4.setSize(200, 30);
-	etiqueta4.setLocation(240, 260);
-	etiqueta4.setFont(new Font("Corbel", Font.BOLD, 18));	
+	etiqueta4.setSize(200, 43);
+	etiqueta4.setLocation(500, 214);
+	etiqueta4.setFont(new Font("Tahoma", Font.PLAIN, 14));	
 	ventanaPanel.add(etiqueta4);
 	
 	JPasswordField password = new JPasswordField();
-	password.setSize(290, 30);
-	password.setLocation(240, 290);
-	password.setFont(new Font("Corbel", Font.BOLD, 18));	
+	password.setSize(200, 19);
+	password.setLocation(500, 260);
+	password.setFont(new Font("Tahoma", Font.PLAIN, 14));	
 	ventanaPanel.add(password);
 	
 	JLabel sexo = new JLabel("Sexo:");
 	sexo.setSize(200, 30);
-	sexo.setLocation(240, 350);
-	sexo.setFont(new Font("Corbel", Font.BOLD, 18));	
+	sexo.setLocation(500, 325);
+	sexo.setFont(new Font("Tahoma", Font.PLAIN, 14));	
 	ventanaPanel.add(sexo);
 	
 	String[] opcionesSexo = {"Masculino", "Femenimo", "Otro..."};
 	JComboBox<String> sexos = new JComboBox<String>(opcionesSexo);
 	sexos.setSize(100,30);
-	sexos.setLocation(300,350);
+	sexos.setLocation(560,325);
 	ventanaPanel.add(sexos);
 	
 	JButton inicioSesion = new JButton("CREAR CUENTA");
@@ -246,6 +246,102 @@ public void login() {
 	inicioSesion.setLocation(160, 400);
 	inicioSesion.setFont(new Font("Corbel", Font.BOLD, 19));	
 	ventanaPanel.add(inicioSesion);
+	JLabel lblNewLabel_1 = new JLabel("Direccion");
+	lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+	lblNewLabel_1.setBounds(50, 140, 112, 35);
+	ventanaPanel.add(lblNewLabel_1);
+
+	JLabel lblNewLabel_2 = new JLabel("Apellido Paterno");
+	lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+	lblNewLabel_2.setBounds(50, 53, 112, 43);
+	ventanaPanel.add(lblNewLabel_2);
+
+	JTextField textField = new JTextField();
+	textField.setBounds(50, 106, 117, 19);
+	ventanaPanel.add(textField);
+	textField.setColumns(10);
+
+	JLabel lblNewLabel_2_1 = new JLabel("Apellido Materno");
+	lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+	lblNewLabel_2_1.setBounds(185, 53, 112, 43);
+	ventanaPanel.add(lblNewLabel_2_1);
+
+	JTextField textField_1 = new JTextField();
+	textField_1.setColumns(10);
+	textField_1.setBounds(180, 106, 117, 19);
+	ventanaPanel.add(textField_1);
+
+	JLabel lblNewLabel_2_1_1 = new JLabel("Nombre/Nombres");
+	lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+	lblNewLabel_2_1_1.setBounds(307, 53, 112, 43);
+	ventanaPanel.add(lblNewLabel_2_1_1);
+
+	JTextField textField_2 = new JTextField();
+	textField_2.setColumns(10);
+	textField_2.setBounds(307, 106, 117, 19);
+	ventanaPanel.add(textField_2);
+
+	JLabel lblNewLabel_1_1 = new JLabel("Datos Personales");
+	lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+	lblNewLabel_1_1.setBounds(10, 10, 145, 43);
+	ventanaPanel.add(lblNewLabel_1_1);
+
+	JTextField textField_3 = new JTextField();
+	textField_3.setBounds(50, 185, 374, 19);
+	ventanaPanel.add(textField_3);
+	textField_3.setColumns(10);
+
+	JLabel lblNewLabel_1_2 = new JLabel("Ciudad");
+	lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+	lblNewLabel_1_2.setBounds(50, 214, 112, 35);
+	ventanaPanel.add(lblNewLabel_1_2);
+
+	JComboBox comboBox = new JComboBox();
+	comboBox.setModel(
+			new DefaultComboBoxModel(new String[] { "Selecciona una", "Cancún", "Chihuahua", "Ciudad de México",
+					"Culiacán", "Guadalajara", "León", "La Paz", "Mérida", "Monterrey", "Puebla", "Querétaro" }));
+	comboBox.setBounds(50, 259, 139, 21);
+	ventanaPanel.add(comboBox);
+
+	JLabel lblNewLabel_1_2_1 = new JLabel("Estado");
+	lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+	lblNewLabel_1_2_1.setBounds(198, 214, 112, 35);
+	ventanaPanel.add(lblNewLabel_1_2_1);
+
+	JComboBox comboBox_1 = new JComboBox();
+	comboBox_1.setModel(new DefaultComboBoxModel(new String[] { "Selecciona una", "Aguascalientes  ",
+			"Baja California  ", "Baja California Sur  ", "Campeche  ", "Chiapas  ", "Chihuahua  ",
+			"Ciudad de México  ", "Coahuila  ", "Colima  ", "Durango  ", "Estado de México  ", "Guanajuato  ",
+			"Guerrero  ", "Hidalgo  ", "Jalisco  ", "Michoacán  ", "Morelos  ", "Nayarit  ", "Nuevo León  ",
+			"Oaxaca  ", "Puebla  ", "Querétaro  ", "Quintana Roo  ", "San Luis Potosí  ", "Sinaloa  ", "Sonora  ",
+			"Tabasco  ", "Tamaulipas  ", "Tlaxcala  ", "Veracruz  ", "Yucatán  ", "Zacatecas  " }));
+	comboBox_1.setBounds(199, 259, 139, 21);
+	ventanaPanel.add(comboBox_1);
+
+	JLabel lblNewLabel_1_2_1_1 = new JLabel("CP");
+	lblNewLabel_1_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+	lblNewLabel_1_2_1_1.setBounds(364, 214, 45, 35);
+	ventanaPanel.add(lblNewLabel_1_2_1_1);
+
+	JTextField textField_4 = new JTextField();
+	textField_4.setColumns(10);
+	textField_4.setBounds(348, 260, 76, 19);
+	ventanaPanel.add(textField_4);
+
+	JLabel lblNewLabel_1_2_2 = new JLabel("CURP");
+	lblNewLabel_1_2_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+	lblNewLabel_1_2_2.setBounds(50, 290, 112, 35);
+	ventanaPanel.add(lblNewLabel_1_2_2);
+
+	JTextField textField_5 = new JTextField();
+	textField_5.setColumns(10);
+	textField_5.setBounds(50, 335, 374, 19);
+	ventanaPanel.add(textField_5);
+
+	
+
+	
+
 	
 	JButton btnNewButton = new JButton("ir al login");
 	btnNewButton.setFont(new Font("Corbel", Font.BOLD, 19));	
